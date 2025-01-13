2066004c63d693bf03c11f0219ea7a043ee149ad
Mallory Row <mallory.row@noaa.gov>
2025-01-13 17:01:03 +0000

    Read in missing data text files

--- ush/send/send_mail_write_messages.py
+++ ush/send/send_mail_write_messages.py
@@ -8,11 +8,40 @@ Run By: scripts/mail/send/exevs_send_mail.sh
 '''
 
 import os
+import datetime
 import sys
 import glob
+import numpy as np
+import pandas as pd
 
 print("BEGIN: "+os.path.basename(__file__))
 
-print("hello")
+# Read in environment variables
+DATA = os.environ['DATA']
+NET = os.environ['NET']
+RUN = os.environ['RUN']
+COMIN = os.environ['COMIN']
+COMOUT = os.environ['COMOUT']
+SENDCOM = os.environ['SENDCOM']
+STEP = os.environ['STEP']
+COMPONENT = os.environ['COMPONENT']
+VDATE = os.environ['VDATE']
+vhr = os.environ['vhr']
+
+# Get sendmail files
+sendmail_file_list = (
+    glob.glob(os.path.join(COMIN, 'prep', '*', '*', 'sendmail', '*'))
+    +
+    glob.glob(os.path.join(COMIN, 'stats', '*', '*', '*', '*', 'sendmail', '*'))
+)
+
+# Read sendmail files
+for sendmail_file in sendmail_file_list:
+    sendmail_file_df = pd.read_csv(
+        sendmail_file, sep=':',header=None, dtype=str,
+        skipinitialspace=True
+    ).T
+    sendmail_file_df.columns = sendmail_file_df.iloc[0]
+    sendmail_file_df = sendmail_file_df.drop(sendmail_file_df.index[0])
 
 print("END: "+os.path.basename(__file__))
